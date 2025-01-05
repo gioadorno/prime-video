af536d343a83546c9ac6b8ade46ca603b8d4c606
gioadorno <adorno.gio@gmail.com>
2025-01-05 00:06:32 -0700

    Update

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   basePath: "/prime-video",
-  output: "export",
   images: {
     domains: ["static.vecteezy.com", "themoviedb.org", "image.tmdb.org"],
   },
